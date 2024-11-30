f71df710c74bf95725a535ecf6cd9c2495b4ee55
Bartek Kryza <bkryza@gmail.com>
2024-11-30 20:17:52 +0100

    Updated ecc URL

--- premake_ecc/gallery.lua
+++ premake_ecc/gallery.lua
@@ -1,5 +1,5 @@
 build_system_files = {"premake5.lua"}
 print_version_cmd = "premake5 --version"
-setup_cmd = "git clone git@github.com:MattBystrin/premake-ecc.git ecc || true"
+setup_cmd = "git clone https://github.com/MattBystrin/premake-ecc.git ecc || true"
 compdb_dir = "."
 generate_compdb_cmd = "premake5 ecc"
