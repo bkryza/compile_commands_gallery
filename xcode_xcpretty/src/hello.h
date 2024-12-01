e58a00ee79e96a8bd4207586a2b76210b7bfae04
Bartek Kryza <bkryza@gmail.com>
2024-12-01 23:16:49 +0100

    Added xcode xcpretty example

---
+++ xcode_xcpretty/src/hello.h
@@ -0,0 +1,10 @@
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+#include <string_view>
+
+struct Hello {
+    auto hello() const -> std::string_view { return "Hello, world!"; }
+};
+
+#endif // __HELLO_H__
